561ee0f01219d07d8e7393c585120380edc9f55f
jhansi358 <jhansirani.profile@gmail.com>
2026-03-12 12:46:24 +0530

    ec2v2

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,29 @@ resource "aws_instance" "example" {
     Name = "Hello Terraform"
   }
 }
+
+#sgp block
+
+resource "aws_security_group" "roboshop-all" {
+     name        = "instance_sg"
+     description = "Allow  all inbound traffic"
+
+     ingress {
+         from_port   = 0
+         to_port     = 0
+         protocol    = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+         description = "all from Internet"
+  } 
+     egress {
+         from_port   = 0
+         to_port     = 0
+         protocol    = "-1" # Semantically equivalent to 'all'
+         cidr_blocks = ["0.0.0.0/0"]
+         description = "Allow all egress traffic"
+  }
+
+     tags = {
+         Name = "instance-sg"
+  }
+}
