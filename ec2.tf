c651d29ef647eb706cf14b1bbc08a55ca4baf923
jhansi358 <jhansirani.profile@gmail.com>
2026-03-12 12:49:32 +0530

    ec2v3

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["aws_security_group.roboshop-all.id"]
 
   tags = {
     Name = "Hello Terraform"
