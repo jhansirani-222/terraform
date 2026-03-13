7f186147ae87344be853b3986bbc2b2e7768d85f
jhansi358 <jhansirani.profile@gmail.com>
2026-03-13 06:57:51 +0530

    conditions

--- conditions/conditions.tf
+++ conditions/conditions.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-0b4f379183e5706b9"
-  instance_type = var.instance_name == MONGODB ? t3.small : t3.micro 
+  instance_type = var.instance_name == "MONGODB" ? "t3.small" : "t3.micro" 
   #vpc_security_group_ids = ["aws_security_group.roboshop-all.id"]
 
   tags = var.tags
